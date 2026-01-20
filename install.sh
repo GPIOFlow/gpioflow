f543a186ae8ef8c7847d7beab0aab38cf39edb53
GPIOFlow <info@gpioflow.com>
2026-01-20 21:44:16 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -44,7 +44,7 @@ SQL_SHA256="1ed790ae59bf18d3cd03e68b89b2ccbce654898d9397f3e20a7d9bab5681bbcc"
 
 
 WAR_URL="https://gpioflow.com/software/latest/war/gpioflow.war"
-WAR_SHA256="be7409dd5c5afd3e232658e88da2a2c76d1f8e8a4cf6290d4ee86da270725f7a"
+WAR_SHA256="a99d41e61b1a4c121e3891a140b608800fc5010ea92ab737c7c152ff8f680d9b"
 # -------------------------
 # DB user 
 # -------------------------
